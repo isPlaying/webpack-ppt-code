8ee4bea95de52e045f8fb3b7a3059f82aa64dddf
isPlaying <369370@gmail.com>
2023-08-06 11:42:11 +0800

    feat: add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { ChangeEvent, useState } from "react";
 import styles from "./index.module.less";
 import { RouteObject, useRoutes } from "react-router-dom";
 
 const ROUTER_CONFIG: RouteObject[] = [
   {
     path: "/",
-    element: <h1>Home</h1>,
+    element: (
+      <>
+        <h1 className={styles.title}>Home</h1>
+        <input placeholder="请输入" />
+      </>
+    ),
   },
   {
     path: "/about",
